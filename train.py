c1ce4c5bfe2b27d797846a7ba24053124093ea4a
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2022-03-07 20:27:25 -0500

    new file for training

---
+++ train.py
@@ -0,0 +1,8 @@
+from ESRGAN import Generator, Discriminator
+from SRCNN import SRCNN
+
+
+# TODO: Discuss which data set to use and implement training function and eval function
+
+def train():
+    pass
